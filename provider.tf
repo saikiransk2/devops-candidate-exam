827c961a1a6208afb388784d5a75d03e304fa3c1
saikiransk2 <sai.kiran@siemens.com>
2023-04-20 13:41:07 -0700

    new provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
-
-# Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    region = "ap-south-1"
+    key    = "sai.kiran"
+  }
+}
