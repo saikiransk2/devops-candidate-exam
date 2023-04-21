dc00cd8a467a9a5d52456e1f500fc927868f9ee1
saikiransk2 <sai.kiran@siemens.com>
2023-04-21 08:49:09 -0700

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,14 +1,19 @@
-import requests
+import http.client
+import json
+import base64
 
-url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
-headers = {"X-Siemens-Auth": "test"}
-payload = {
-    "subnet_id": "nat-04bad8be564a37c70",
-    "name": "sai.kiran",
-    "email": "sai.kiran@siemens.com"
-}
+def lambda_handler(event, context):
+    url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+    headers = {"X-Siemens-Auth": "test"}
+    payload = {
+        "subnet_id": "subnet-04625301f1d2eee6a",
+        "name": "sai.kiran",
+        "email": "sai.kiran@siemens.com"
+    }
+    
+    data = json.dumps(payload)
 
-response = requests.post(url, headers=headers, json=payload)
-
-print(response.status_code)
-print(response.text)
+    conn = http.client.HTTPSConnection(url)
+    conn.request('POST', '/post', data, headers)
+    response = conn.getresponse()
+    print(response.read().decode())
